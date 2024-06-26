e252ad09173a0f12e0668b227a8ae753c4c5d78b
Eros Mendonça <erosmendonca@outlook.com>
2024-06-27 00:25:27 +0100

    feat: express

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-console.log("hello world")
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
