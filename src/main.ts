1ec00ef6dca8c4cbda7483b47d8c52fa7ef37643
Eros Mendonça <erosmendonca@outlook.com>
2024-07-10 00:14:21 +0100

    feat: reading routes recursively

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,46 @@
 import express, { Express, Request, Response } from "express";
-import { readFile } from "fs";
+import { readFile, readdir } from "fs";
 import { join } from "path";
 
 const app: Express = express();
 const port = 3000;
-const routesPath = "./routes";
 
-const filePath = join(__dirname, routesPath, "index.html");
+const routesPath = "routes";
 
-readFile(filePath, "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  
-  app.get("/", (req: Request, res: Response) => {
-    res.send(data);
-  }); 
-});
+function getRoutes(path: string) {
+  const joinedPath = join(__dirname, path);
+  readdir(joinedPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    data.forEach((item) => {
+      const isDirectory = !item.includes(".");
+
+      if (isDirectory) {
+        const newPath = join(path, item);
+        getRoutes(newPath);
+      } else {
+        const filePath = join(joinedPath, item);
+        console.log(`Loaded route: ${path}`);
+
+        readFile(filePath, "utf8", (err, data) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          app.get(`/${path}`, (req: Request, res: Response) => {
+            res.send(data);
+          });
+        });
+      }
+    });
+  });
+}
+
+getRoutes(routesPath);
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
